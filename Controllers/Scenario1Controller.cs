81bf8090d2584d5a94cd33d34153be74181533ff
Jerrie Pelser <jerrie@jerriepelser.com>
2018-08-14 14:47:31 +0700

    Initial commit

---
+++ Controllers/Scenario1Controller.cs
@@ -0,0 +1,28 @@
+﻿using DemoApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class Scenario1Controller : Controller
+    {
+        private readonly Database _database;
+
+        public Scenario1Controller(Database database)
+        {
+            _database = database;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_database.Customers);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _database.Customers.RemoveAll(customer => customer.Id == id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Data/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoApp.Data
+{
+    public class Customer
+    {
+        public string Avatar { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public int Id { get; set; }
+        public string LastName { get; set; }
+    }
+}
