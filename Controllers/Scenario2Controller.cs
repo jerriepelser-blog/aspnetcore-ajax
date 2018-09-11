f792aabbafa3b8d16a311cc90214888371c219cd
Jerrie Pelser <jerrie@jerriepelser.com>
2018-09-11 12:00:06 +0700

    Add Scenario 2

---
+++ Controllers/Scenario2Controller.cs
@@ -0,0 +1,28 @@
+﻿using DemoApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class Scenario2Controller : Controller
+    {
+        private readonly Database _database;
+
+        public Scenario2Controller(Database database)
+        {
+            _database = database;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_database.Customers);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _database.Customers.RemoveAll(customer => customer.Id == id);
+
+            return PartialView("_CustomerList", _database.Customers);
+        }
+    }
+}
